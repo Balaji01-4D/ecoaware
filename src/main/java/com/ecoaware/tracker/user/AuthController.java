b11f5eb702f911ce5aa18fec985f05869eca087d
Balaji01-4D <j.balaji2468@gmail.com>
2025-06-23 22:26:28 +0530

    feat: more refined function

--- src/main/java/com/ecoaware/tracker/user/AuthController.java
+++ src/main/java/com/ecoaware/tracker/user/AuthController.java
@@ -1,15 +1,19 @@
 package com.ecoaware.tracker.user;
 
+import com.ecoaware.tracker.security.JwtService;
 import com.ecoaware.tracker.user.dao.AuthenticationResponse;
 import com.ecoaware.tracker.user.dao.LoginRequest;
 import com.ecoaware.tracker.user.dao.RegisterRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,15 +22,29 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class AuthController {
 
     private final UserService userService;
+    private final JwtService jwtService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest) {
-        return ResponseEntity.ok(userService.register(registerRequest));
+        Users user = userService.fromRegisterRequestDto(registerRequest);
+        var registeredUser = userService.register(user);
+        String token = jwtService.generateToken(new HashMap<>(), new UserPrincipal(registeredUser));
+        return ResponseEntity.ok(userService.toAuthenticationResponseDto(token));
     }
 
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody LoginRequest loginRequest) {
-        return ResponseEntity.ok(userService.authenticate(loginRequest));
+        Users user = new Users();
+        user.setEmail(loginRequest.getEmail());
+        user.setPassword(loginRequest.getPassword());
+
+        if (userService.authenticate(user)){
+            String token = jwtService.generateToken(new HashMap<>(), new UserPrincipal(user));
+            return ResponseEntity.ok(userService.toAuthenticationResponseDto(token));
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
     }
 }

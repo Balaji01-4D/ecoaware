d07f6ba10c54b2f233f3cbb01d9fb8c26957b2d0
Balaji01-4D <j.balaji2468@gmail.com>
2025-06-25 23:15:08 +0530

    feat: add test for complaint service

---
+++ src/test/java/com/ecoaware/tracker/complaint/ComplaintServiceTest.java
@@ -0,0 +1,36 @@
+package com.ecoaware.tracker.complaint;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComplaintServiceTest {
+
+    @Test
+    void getAllComplaints() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getComplaintsById() {
+    }
+
+    @Test
+    void convertToResponseDto() {
+    }
+
+    @Test
+    void deleteComplaintById() {
+    }
+
+    @Test
+    void updateFromRequestDto() {
+    }
+
+    @Test
+    void saveComplaint() {
+    }
+}
